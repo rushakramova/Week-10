aea08777a133a5bb6573cef1b980e0f7bc5ae3e9
rushakramova <rushemz@yandex.ru>
2023-05-14 16:46:47 +0400

    add files

---
+++ slider.js
@@ -0,0 +1,24 @@
+let offset = 0; //смещение от левого края
+const sliderLine = document.querySelector('.slider-line');
+
+document.querySelector('.slider-prev') .addEventListener ('click', function(){
+  offset = offset - 256;
+  if (offset < 0){
+    offset = 768;
+  }
+  sliderLine.style.left = - offset + 'px';
+  
+  });
+
+document.querySelector('.slider-next') .addEventListener ('click', function(){
+offset = offset + 256;
+if (offset > 768){
+  offset = 0;
+}
+sliderLine.style.left = - offset + 'px';
+});
+
+function LearnJS() {
+  console.log('Я учу JavaScript!');
+}
+LearnJS();
